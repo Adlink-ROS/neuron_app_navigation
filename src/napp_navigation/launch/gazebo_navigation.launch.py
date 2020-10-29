a69c10af1c3492ecb50e7e88d6a7b6e26645d50f
ChenYing Kuo <evshary@gmail.com>
2020-10-29 10:15:05 +0800

    Able to run navigation2 on Gazebo in one launch file

--- src/napp_navigation/launch/gazebo_navigation.launch.py
+++ src/napp_navigation/launch/gazebo_navigation.launch.py
@@ -2,41 +2,38 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (IncludeLaunchDescription, GroupAction)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import ExecuteProcess
-from launch.actions import GroupAction
 from launch.substitutions import LaunchConfiguration
 
+import pdb
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
-    world = [get_package_share_directory('neuronbot2_gazebo'), '/worlds/']
-    world.append(LaunchConfiguration('world_model', default='mememan_world.model'))
+    # Path
     gazebo_launch_dir = os.path.join(get_package_share_directory('neuronbot2_gazebo'), 'launch')
-    gazebo_model_path = os.path.join(get_package_share_directory('neuronbot2_gazebo'), 'models')
+    nb2nav_launch_dir = os.path.join(get_package_share_directory('neuronbot2_nav'), 'launch')
+    nb2nav_map_dir = os.path.join(get_package_share_directory('neuronbot2_nav'), 'map')
 
-    if 'GAZEBO_MODEL_PATH' in os.environ:
-        os.environ['GAZEBO_MODEL_PATH'] += ":" + gazebo_model_path
-    else :
-        os.environ['GAZEBO_MODEL_PATH'] = gazebo_model_path
+    # Parameters
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    open_rviz = LaunchConfiguration('open_rviz', default='True')
+    ## mememan_world.model / phenix_world.model
+    world_model = LaunchConfiguration('world_model', default='mememan_world.model')
+    ## mememan.yaml / phenix.yaml
+    map_path = LaunchConfiguration('map', default=nb2nav_map_dir+'/mememan.yaml')
 
-    gazebo_bringup = GroupAction([
-        ExecuteProcess(
-            cmd=['gzserver', '--verbose', world , '-s', 'libgazebo_ros_init.so'],
-            output='screen'),
-
-        ExecuteProcess(
-            cmd=['gzclient'],
-            output='screen'),
-
-        ExecuteProcess(
-            cmd=['ros2', 'param', 'set', '/gazebo', 'use_sim_time', use_sim_time],
-            output='screen'),
+    neuron_app_bringup = GroupAction([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(gazebo_launch_dir, 'neuronbot2_world.launch.py')),
+            launch_arguments={'use_sim_time': use_sim_time,
+                              'world_model': world_model}.items()),
 
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(gazebo_launch_dir, 'robot_state_publisher.launch.py')),
-            launch_arguments={'use_sim_time': use_sim_time}.items()),
+            PythonLaunchDescriptionSource(os.path.join(nb2nav_launch_dir, 'bringup_launch.py')),
+            launch_arguments={'use_sim_time': use_sim_time,
+                              'open_rviz': open_rviz,
+                              'map': map_path}.items()),
     ])
+
     ld = LaunchDescription()
-    ld.add_action(gazebo_bringup)
+    ld.add_action(neuron_app_bringup)
     return ld
